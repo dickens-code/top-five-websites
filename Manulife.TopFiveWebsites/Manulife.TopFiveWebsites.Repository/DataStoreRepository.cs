b2355bbc92fadafad6209b5f026526d52c7d028d
Dickens <dickens.code@gmail.com>
2017-07-06 00:07:43 +0800

    fix "missing EntityFramework.SqlServer.dll" issue

--- Manulife.TopFiveWebsites/Manulife.TopFiveWebsites.Repository/DataStoreRepository.cs
+++ Manulife.TopFiveWebsites/Manulife.TopFiveWebsites.Repository/DataStoreRepository.cs
@@ -15,6 +15,9 @@ namespace Manulife.TopFiveWebsites.Repository
 
         public DataStoreRepository(TopFiveWebsitesEntities entities)
         {
+            //fix as suggested in https://social.msdn.microsoft.com/Forums/en-US/b348a0c2-94d9-4db5-a041-b81a97e76b3f/entityframeworksqlserver-not-deploying?forum=adodotnetentityframework
+            var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+
             _entities = entities;
             _entities.Database.CommandTimeout = 60;
         }
