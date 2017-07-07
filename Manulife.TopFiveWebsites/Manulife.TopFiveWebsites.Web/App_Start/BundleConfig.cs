b18ddeca65962cd350d0243304ff4b0b3f3724ff
Dickens <dickens.code@gmail.com>
2017-07-08 01:46:05 +0800

    fix woff/woff2 error, add link style for As Of date

--- Manulife.TopFiveWebsites/Manulife.TopFiveWebsites.Web/App_Start/BundleConfig.cs
+++ Manulife.TopFiveWebsites/Manulife.TopFiveWebsites.Web/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace Manulife.TopFiveWebsites.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/app.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include(
                       "~/Scripts/jquery-{version}.js",
